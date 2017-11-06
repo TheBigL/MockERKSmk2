09bb99a3006581f8e4dd72f6579680b7b703cbad
TheBigL <lmohamed1@studentmail.nait.ca>
2017-11-05 18:02:41 -0700

    Created Erks.FrameWork plus DBContext + WCString

---
+++ MockERKS.Framework/DAL/MockErksDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockERKS.Framework.DAL
+{
+    class MockErksDbContext
+    {
+    }
+}
---
+++ MockERKS.Framework/DAL/Security/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockERKS.Framework.DAL.Security
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext() : base("name=MockERKSDb")
+        { }
+    }
+}
