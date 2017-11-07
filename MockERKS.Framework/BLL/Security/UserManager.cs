d3e362b52fa4ed1efe5d01f4c0a589c647450290
squadri2 <sayedquadri91@hotmail.com>
2017-11-07 15:02:43 -0700

    Addition of WebMaster

--- MockERKS.Framework/BLL/Security/UserManager.cs
+++ MockERKS.Framework/BLL/Security/UserManager.cs
@@ -17,10 +17,7 @@ namespace MockERKS.Framework.BLL.Security
     public class UserManager : UserManager<ApplicationUser>
     {
 
-        public UserManager()
-            : base(new UserStore<ApplicationUser>(new ApplicationDbContext()))
-        {
-        }
+     
 
         #region Constants
         private const string STR_DEFAULT_PASSWORD = "Pa$$word1";
@@ -30,9 +27,47 @@ namespace MockERKS.Framework.BLL.Security
         private const string STR_EMAIL_FORMAT = "{0}@Mockerks.ca";
         private const string STR_WEBMASTER_USERNAME = "Webmaster";
         #endregion
+        public UserManager()
+         : base(new UserStore<ApplicationUser>(new ApplicationDbContext()))
+        {
+        }
+
+        public void AddWebMaster()
+        {
+            //Users accesses all the records on the AspNetUsers table
+            //UserName is the user logon user id (dwelch)
+            if (!Users.Any(u => u.UserName.Equals(STR_WEBMASTER_USERNAME)))
+            {
+                //create a new instance that will be used as the data to
+                //   add a new record to the AspNetUsers table
+                //dynamically fill two attributes of the instance
+                var webmasterAccount = new ApplicationUser()
+                {
+                    UserName = STR_WEBMASTER_USERNAME,
+                    Email = string.Format(STR_EMAIL_FORMAT, STR_WEBMASTER_USERNAME)
+                };
+
+                //place the webmaster account on the AspNetUsers table
+                this.Create(webmasterAccount, STR_DEFAULT_PASSWORD);
+
+                //place an account role record on the AspNetUserRoles table
+                //.Id comes from the webmasterAccount and is the pkey of the Users table
+                //role will comes from the Entities.Security.SecurityRoles
+                this.AddToRole(webmasterAccount.Id, SecurityRoles.WebAdmins);
+            }
+        }
 
 
         #region User CRUD
+
+
+
+
+
+
+
+
+
         [DataObjectMethod(DataObjectMethodType.Select, true)]
         public List<UserProfile> ListAllStaff()
         {
