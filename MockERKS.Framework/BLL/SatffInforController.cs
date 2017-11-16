09c9bc762e8a3b2b98a01c15ea6b16cb3616efac
wzhang28 <wzhang28@studentmail.nait.ca>
2017-11-16 13:49:41 -0700

    Create the controller for the satff Information form page

---
+++ MockERKS.Framework/BLL/SatffInforController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using MockERKS.Framework.DAL;
+using MockERKS.Framework.Entities;
+using System.ComponentModel;
+#endregion
+
+namespace MockERKS.Framework.BLL
+{
+    [DataObject]
+    public class SatffInforController
+    {
+        
+    }
+}
