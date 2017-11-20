b19a2dec880abdf9835e225be1deb4d0e646ae9b
wzhang28 <wzhang28@studentmail.nait.ca>
2017-11-20 15:37:20 -0700

    Function to generate list of category

--- MockERKS.Framework/BLL/SatffInforController.cs
+++ MockERKS.Framework/BLL/SatffInforController.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using MockERKS.Framework.DAL;
 using MockERKS.Framework.Entities;
 using System.ComponentModel;
+using MockERKS.Framework.Entities.POCOs;
 #endregion
 
 namespace MockERKS.Framework.BLL
@@ -15,6 +16,21 @@ namespace MockERKS.Framework.BLL
     [DataObject]
     public class SatffInforController
     {
-        
+        /* Author: Wenyu Zhang */
+        /* Founction: Catch the list of Category to show in the dropdown list */
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<CategoryList> DropDownCategory()
+        {
+            using (var context = new MockERKSDb())
+            {
+                var category = from categories in context.Categories
+                               select new CategoryList
+                               {
+                                   categoryID = categories.Category_ID,
+                                   categoryName = categories.Category_Name
+                               };
+                return category.ToList();
+            }
+        }
     }
 }
