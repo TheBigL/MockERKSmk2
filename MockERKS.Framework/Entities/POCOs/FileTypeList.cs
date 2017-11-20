6493c6cc6006006758ecd340099d07e11e15e1c8
wzhang28 <wzhang28@studentmail.nait.ca>
2017-11-20 15:43:07 -0700

    POCOs' FileTypeList

---
+++ MockERKS.Framework/Entities/POCOs/FileTypeList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockERKS.Framework.Entities.POCOs
+{
+    public class FileTypeList
+    {
+        public int Type_ID { get; set; }
+        public string Type_Description { get; set; }
+    }
+}
