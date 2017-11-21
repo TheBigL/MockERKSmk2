8f2850a20cfd1296d797840231cba6ef60bb1b64
squadri2 <sayedquadri91@hotmail.com>
2017-11-20 17:04:15 -0700

    Registration

--- MockERKS.Framework/Entities/POCOs/OrganizationDescription.cs
+++ MockERKS.Framework/Entities/POCOs/OrganizationDescription.cs
@@ -8,6 +8,7 @@ namespace MockERKS.Framework.Entities.POCOs
 {
     public class OrganizationDescription
     {
-        public string organizationDescription { get; set; }
+        public string Organization_Description { get; set; }
+        public int Organization_ID { get; set; }
     }
 }
--- Website/Account/Register.aspx.cs
+++ Website/Account/Register.aspx.cs
@@ -9,20 +9,21 @@ using System.Data.SqlClient;
 
 public partial class Account_Register : Page
 {
-    
-   
+
+    SqlCommand cmd = new SqlCommand();
+    SqlConnection sysmgr = new SqlConnection();
 
     protected void CreateUser_Click(object sender, EventArgs e)
     {
-        ClientController sysmgr = new ClientController();
-        Organization neworg = new Organization();
-        neworg.Organization_Name = OrganizationName.Text;
-        neworg.Organization_Description = Description.SelectedValue;
-        neworg.Phone = Convert.ToInt32(this.Phone.Text);
-        neworg.Email = Email.Text;
 
+        SqlCommand cmd = new SqlCommand("insert into Organization" + "Organization_Name,Email,Phone,Description)values (@Organization_Name,@Email,@Phone,@Description)",sysmgr);
+        cmd.Parameters.AddWithValue("@Organization_Name", OrganizationName.Text);
+        cmd.Parameters.AddWithValue("@Email", Email.Text);
+        cmd.Parameters.AddWithValue("@Description", Description.SelectedItem.Value);
+        cmd.Parameters.AddWithValue("@Phone", OrganizationName.Text);
+        cmd.ExecuteNonQuery();
 
-       var manager = new UserManager();
+        var manager = new UserManager();
         var user = new ApplicationUser() { UserName = UserName.Text };
         IdentityResult result = manager.Create(user, Password.Text);
         if (result.Succeeded)
