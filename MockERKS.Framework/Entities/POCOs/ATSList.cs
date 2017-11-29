2aa946016fe594b6e6b8b388f4b8bb10327ecbca
wzhang28 <wzhang28@studentmail.nait.ca>
2017-11-29 15:44:08 -0700

    Create POCO ATS List

---
+++ MockERKS.Framework/Entities/POCOs/ATSList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockERKS.Framework.Entities.POCOs
+{
+    public class ATSList
+    {
+        public int ATSID { get; set; }
+        public string meridian { get; set; }
+        public string range { get; set; }
+        public string township { get; set; }
+        public string section { get; set; }
+        public string quarterSection { get; set; }
+        public string lsd { get; set; }
+    }
+}
