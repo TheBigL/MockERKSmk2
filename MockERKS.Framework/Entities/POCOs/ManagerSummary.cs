2508b13c7778463b24f1108b5c0f7e21ac3659cf
TheBigL <lmohamed1@studentmail.nait.ca>
2017-11-29 14:55:48 -0700

    Added Manager POCO object

---
+++ MockERKS.Framework/Entities/POCOs/ManagerSummary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockERKS.Framework.Entities.POCOs
+{
+    public class ManagerSummary
+    {
+        public int managerID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+    }
+}
