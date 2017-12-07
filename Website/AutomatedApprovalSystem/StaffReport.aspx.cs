0b9c2586ebd146d2e0fe80b783551298cbb490f8
TheBigL <lmohamed1@studentmail.nait.ca>
2017-12-07 14:40:42 -0700

    Added Security to the File Report

--- Website/AutomatedApprovalSystem/StaffReport.aspx.cs
+++ Website/AutomatedApprovalSystem/StaffReport.aspx.cs
@@ -9,7 +9,15 @@ public partial class AutomatedApprovalSystem_Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!Request.IsAuthenticated)
+        {
+            Response.Redirect("~/Account/Login.aspx");
+        }
 
+        else if (User.IsInRole("Client"))
+        {
+            Response.Redirect("~/Default.aspx");
+        }
     }
 
     protected void BackToLookup_Click(object sender, EventArgs e)
