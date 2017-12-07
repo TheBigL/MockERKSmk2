da4b3cfe008f12b5a5d70db1ab066ce10529a1dd
wzhang28 <wzhang28@studentmail.nait.ca>
2017-12-07 16:49:24 -0700

    Upload File Page

---
+++ Website/AutomatedApprovalSystem/UploadPDF.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="UploadPDF.aspx.cs" Inherits="AutomatedApprovalSystem_UploadPDF" %>
+
+<%@ Register Src="~/UserControls/MessageUserControl.ascx" TagPrefix="uc1" TagName="MessageUserControl" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" Runat="Server">
+
+    <br />
+    <br />
+    <asp:Button CssClass="btn" ID="Back" runat="server" Text="Back" OnClick="Back_Click" />
+    <br />
+    <br />
+
+    <asp:Table runat="server">
+        <asp:TableRow>
+            <asp:TableCell>Select File</asp:TableCell>
+
+            <asp:TableCell>
+                <asp:FileUpload ID="FileUpload" runat="server" ToolTip="Select Only PDF File" />
+            </asp:TableCell>
+
+            <asp:TableCell>
+                <asp:Button ID="Upload" runat="server" Text="Upload" OnClick="Upload_Click" />
+            </asp:TableCell>
+
+            <asp:TableCell>
+                <asp:Button ID="View" runat="server" Text="View Files" OnClick="View_Click" />
+            </asp:TableCell>
+        </asp:TableRow>
+    </asp:Table>
+
+    <asp:Table runat="server">
+        <asp:TableRow>
+            <asp:TableCell>
+                <p>
+                    <asp:Label ID="Label2" runat="server"></asp:Label>
+                </p>
+            </asp:TableCell>
+        </asp:TableRow>
+    </asp:Table>
+
+    <asp:GridView ID="Gridview1" runat="server" Caption="PDF Files" CaptionAlign="Top" HorizontalAlign="Justify" DataKeyNames="id" OnSelectedIndexChanged="Gridview1_SelectedIndexChanged" ToolTip="PDF File Download Tool" CellPadding="4" ForeColor="#333333" GridLines="None">
+        <RowStyle BackColor="#E3EAEB" />
+        <Columns>
+            <asp:CommandField ShowSelectButton="True" SelectText="Download" ControlStyle-ForeColor="Blue" />
+        </Columns>
+        <FooterStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />  
+        <PagerStyle BackColor="#666666" ForeColor="White" HorizontalAlign="Center" />  
+        <SelectedRowStyle BackColor="#C5BBAF" Font-Bold="True" ForeColor="#333333" />  
+        <HeaderStyle BackColor="Gray" Font-Bold="True" ForeColor="White" />  
+        <EditRowStyle BackColor="#7C6F57" />  
+        <AlternatingRowStyle BackColor="White" />
+    </asp:GridView>
+
+
+</asp:Content>
+
---
+++ Website/AutomatedApprovalSystem/UploadPDF.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AutomatedApprovalSystem_UploadPDF : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Back_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("ClientPage.aspx");
+    }
+
+    protected void Upload_Click(object sender, EventArgs e)
+    {
+        //Label2.Visible = true;
+        //string filePath = FileUpload.PostedFile.FileName;
+        //string filename1 = Path.GetFileName(filePath);
+        //string ext = Path.GetExtension(filename1);
+        //string type = String.Empty;
+
+        //if (FileUpload.HasFile)
+        //{
+        //    try
+        //    {
+        //        switch (ext)
+        //        {
+        //            case ".pdf":
+        //                type = "application/pdf";
+        //                break;
+        //        }
+
+        //        if (type != String.Empty)
+        //        {
+        //            connection();
+        //            Stream fs = FileUpload.PostedFile.InputStream;
+        //            BinaryReader br = new BinaryReader(fs);
+        //            Byte[] bytes = br.ReadBytes((Int32)fs.Length);
+        //            query = "insert into PDFFiles (Name,type,data)" + " values (@Name, @type, @Data)";
+        //            com = new SqlCommand(query, con);
+        //            com.Parameters.Add("@Name", SqlDbType.VarChar).Value = filename1;
+        //            com.Parameters.Add("@type", SqlDbType.VarChar).Value = type;
+        //            com.Parameters.Add("@Data", SqlDbType.Binary).Value = bytes;
+        //            com.ExecuteNonQuery();
+        //            Label2.ForeColor = System.Drawing.Color.Green;
+        //            Label2.Text = "File Uploaded Successfully";
+        //        }
+        //        else
+        //        {
+        //            Label2.ForeColor = System.Drawing.Color.Red;
+        //            Label2.Text = "Select Only PDF Files ";
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Label2.Text = "Error: " + ex.Message.ToString();
+        //    }
+        //}
+    }
+
+    protected void View_Click(object sender, EventArgs e)
+    {
+        //connection();
+        //query = "Select *from PDFFiles";
+        //SqlDataAdapter da = new SqlDataAdapter(query, con);
+        //DataSet ds = new DataSet();
+        //da.Fill(ds);
+        //GridView1.DataSource = ds;
+        //GridView1.DataBind();
+        //con.Close();
+    }
+
+    protected void Gridview1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        //connection();
+        //SqlCommand com = new SqlCommand("select Name,type,data from PDFFiles where id=@id", con);
+        //com.Parameters.AddWithValue("id", GridView1.SelectedRow.Cells[1].Text);
+        //SqlDataReader dr = com.ExecuteReader();
+        //if (dr.Read())
+        //{
+        //    Response.Clear();
+        //    Response.Buffer = true;
+        //    Response.ContentType = dr["type"].ToString();
+        //    Response.AddHeader("content-disposition", "attachment;filename=" + dr["Name"].ToString());
+        //    Response.Charset = "";
+        //    Response.Cache.SetCacheability(HttpCacheability.NoCache);
+        //    Response.BinaryWrite((byte[])dr["data"]);
+        //    Response.End();
+        //}
+    }
+}
